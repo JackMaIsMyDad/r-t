596a6153afbdd392d11f4b2e02cb28ee58dd25de
JackMaIsMyDad <60655273+JackMaIsMyDad@users.noreply.github.com>
2020-12-20 16:28:38 +0800

    change portal

--- src/view/portal/index.js
+++ src/view/portal/index.js
@@ -1,4 +1,4 @@
-import { Radio, Form, DatePicker, Button, Checkbox } from "antd";
+import { Form, DatePicker, Button, Input } from "antd";
 import React, { Component } from "react";
 
 const { RangePicker } = DatePicker;
@@ -38,55 +38,15 @@ class Portal extends Component {
           layout="inline"
           initialValues={{ remember: true }}
         >
-          <Form.Item label="报表类型" name="reportType">
-            <Radio.Group>
-              <Radio value={0}>日报</Radio>
-              <Radio value={1} disabled={true}>
-                月报
-              </Radio>
-            </Radio.Group>
+          <Form.Item label="fileName" name="fileName">
+            <Input placeholder="fileName" />
           </Form.Item>
-          <Form.Item
-            label="日期范围"
-            name="searchDate"
-            wrapperCol={{ span: 18 }}
-          >
+          <Form.Item label="dateRange" wrapperCol={{ span: 18 }}>
             <RangePicker />
-          </Form.Item>
-          <Form.Item
-            label="平台统计方式"
-            labelCol={{ span: 10 }}
-            name="statisDateDimType"
-          >
-            <Radio.Group>
-              <Radio value={0}>按汇总</Radio>
-              <Radio value={1} disabled={true}>
-                按明细
-              </Radio>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item
-            label="用户来源"
-            name="checkedList"
-            wrapperCol={{ span: 24 }}
-          >
-            <Checkbox
-              indeterminate={this.indeterminate}
-              onChange={this.onCheckAllChange}
-              checked={this.checkAll}
-              style={{ marginRight: "8px" }}
-            >
-              全选
-            </Checkbox>
-            <Checkbox.Group
-              options={plainOptions}
-              value={this.state.checkedList}
-              onChange={this.onChange}
-            ></Checkbox.Group>
           </Form.Item>
           <Form.Item>
             <Button type="primary" htmlType="submit">
-              Submit
+              Search
             </Button>
           </Form.Item>
         </Form>
